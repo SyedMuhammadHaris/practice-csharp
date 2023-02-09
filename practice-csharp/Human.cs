e809f524ae3385a80cfaa4c6e5daf025733e08f8
SyedMuhammadHaris <mohammadharisahmed.18@gmail.com>
2023-02-09 19:13:40 +0500

    oop learning first phase

---
+++ practice-csharp/Human.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice_csharp
+{
+    internal class Human
+    {
+        //string name = "Hhhh";
+        //public string firstName;
+        //public string lastName;
+        private string firstName;
+        private string lastName;
+        private int age;
+        private string eyeColor;
+
+        //public void printName(string firstName, string lastName)
+        //{
+        //    Console.WriteLine("My name is {0} {1}",firstName,lastName);
+        //}
+        //We can create in multiple contructors in C# of same name
+
+        //default Contructor 1
+        public Human()
+        {
+            Console.WriteLine("Constructor is caled Object is created");
+        }
+       // Contructor 2
+        public Human(string myFirstName)
+        {
+            firstName = myFirstName;
+        }
+        //Contructor 3
+        public Human(string myFirstName, string lastName, int age)
+        {
+            firstName = myFirstName;
+            this.lastName = lastName;
+            this.age = age;
+        }
+        //Contructor 4 in C#
+        public Human(string myFirstName,string lastName, int age, string eyeColor)
+        {
+            firstName = myFirstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.eyeColor = eyeColor;
+        }
+
+        public void IntrroduceYourSelf()
+        {
+            if (firstName != null && lastName != null && eyeColor != null && age != 0)
+            {
+                if (age == 1)
+                {
+                    Console.WriteLine("My name is {0} {1} and {2} year and my eye color is {3}", firstName, lastName, age, eyeColor);
+                }
+                else
+                    Console.WriteLine("My name is {0} {1} and {2} years and my eye color is {3}", firstName, lastName, age, eyeColor);
+            }
+            else
+                if(firstName != null && lastName != null && age != 0)
+            {
+                Console.WriteLine("\"My name is {0} {1} and {2} years", firstName,lastName,age);
+            }
+            else
+                 if (firstName != null)
+            {
+                Console.WriteLine("Your first name is : {0}", firstName);
+            }
+            else
+                Console.WriteLine("Empty Constructor");
+        }
+
+        
+    }
+}
--- practice-csharp/Program.cs
+++ practice-csharp/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
+using practice_csharp;
 using System;
 
 
@@ -12,6 +13,28 @@ namespace HelloWorldApplication
 
         public static void Main()
         {
+            //Human Sarim = new Human();
+            //Sarim.firstName = "Sarim";
+            //Sarim.lastName = "Ali";
+            //Sarim.printName();
+
+            Human mubariz = new Human("Mubariz", "Shaikh",23,"black"); //here we pass contructor values create in HUMAN class
+            mubariz.IntrroduceYourSelf();
+
+            Human sarim = new Human("Sarim", "Ali", 1,"Green");
+            sarim.IntrroduceYourSelf();
+            Human fahad = new Human("Fahad","Shamsi",24);
+            fahad.IntrroduceYourSelf();
+
+            Human Harry = new Human("Harry");
+            Harry.IntrroduceYourSelf();
+            //mubariz.firstName = "Mubariz";
+            //mubariz.lastName = "Shaikh";
+            //mubariz.printName("Mubariz","Shaikh");
+
+            Human basicHuman = new Human();
+            basicHuman.IntrroduceYourSelf();
+
 
             //Main();
         }
